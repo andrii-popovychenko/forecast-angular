cfdddccc55f5fe80a7921dc16336886d6a5aa536
Andrii Popovychenko <andrii.popovichenko@contractors.roche.com>
2021-12-13 18:38:21 +0200

    Added first container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './containers/mainPage/mainPage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/containers/mainPage/mainPage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-page',
+  templateUrl: './mainPage.component.html',
+  styleUrls: ['./mainPage.component.scss'],
+})
+export class MainPageComponent {}
