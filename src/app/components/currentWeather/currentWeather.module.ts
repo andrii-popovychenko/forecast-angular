4af0e5f35c82a289f7f8634a66afc4b5484dc9f0
Andrii Popovychenko <andrii.popovichenko@contractors.roche.com>
2021-12-15 15:08:43 +0200

    Add pipe for current weather

--- src/app/components/currentWeather/currentWeather.module.ts
+++ src/app/components/currentWeather/currentWeather.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { WeatherConditionsPipe } from 'src/app/pipes/weatherConditions.pipe';
 import { CurrentWeatherComponent } from './currentWeather.component';
 
 @NgModule({
-    declarations: [CurrentWeatherComponent],
+    declarations: [CurrentWeatherComponent, WeatherConditionsPipe],
     exports: [CurrentWeatherComponent],
 })
 export class CurrentWeatherModule {}
