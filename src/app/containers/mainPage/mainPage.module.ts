697c83983475883716216ba0ab86ca26cfa3a555
Andrii Popovychenko <andrii.popovichenko@contractors.roche.com>
2022-05-21 20:23:19 +0300

    Add header component

--- src/app/containers/mainPage/mainPage.module.ts
+++ src/app/containers/mainPage/mainPage.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CurrentWeatherModule } from 'src/app/components/currentWeather/currentWeather.module';
 import { DailyWeatherModule } from 'src/app/components/dailyWeather/dailyWeather.module';
+import { HeaderModule } from 'src/app/components/header/header.component.module';
 import { LocationInputModule } from 'src/app/components/locationSelector/locationInput.module';
 import { WeatherProviderModule } from 'src/app/services/weatherProvider/weatherProvider.module';
 import { WeatherProviderService } from 'src/app/services/weatherProvider/weatherProvider.service';
@@ -9,6 +10,7 @@ import { MainPageComponent } from './mainPage.component';
 @NgModule({
     declarations: [MainPageComponent],
     imports: [
+        HeaderModule,
         LocationInputModule,
         CurrentWeatherModule,
         WeatherProviderModule,
